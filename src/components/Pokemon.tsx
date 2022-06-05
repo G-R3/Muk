02625f773df2a78d7b3455ddd2b3902ce244a556
Gerardo <gr261212@gmail.com>
2022-06-05 15:59:20 -0400

    refetch on skip and correct guess

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,21 +1,20 @@
 import Image from "next/image";
-import { useState } from "react";
 
 export default function Pokemon({
   id,
   name,
   sprite,
+  correct,
 }: {
   id: number;
   name: string;
   sprite: string;
+  correct: boolean;
 }): JSX.Element {
-  const [value, setValue] = useState<string>("");
-  const [correct, setCorrect] = useState<boolean>(false);
-
   return (
     <>
       <Image
+        key={id}
         src={sprite}
         alt={name}
         width={256}
@@ -30,20 +29,6 @@ export default function Pokemon({
       >
         {correct ? name : ""}
       </h1>
-
-      <div className="flex items-center gap-5">
-        <input
-          type="text"
-          className={`border-2 p-2 rounded-md`}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <button className="text-2xl border-2 rounded-full w-10 h-10">
-          &rarr;
-        </button>
-
-        <button>Skip</button>
-      </div>
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,65 @@
 import type { NextPage } from "next";
 import { trpc } from "../../utils/trpc";
+import { useState, useRef } from "react";
 import Pokemon from "../components/Pokemon";
 
 const Home: NextPage = () => {
-  const { data, error, isLoading } = trpc.useQuery(["get-pokemon"], {
+  const [value, setValue] = useState<string>("");
+  const [correct, setCorrect] = useState<boolean>(false);
+  let timerRef = useRef<NodeJS.Timeout>();
+
+  const { data, error, isLoading, refetch } = trpc.useQuery(["get-pokemon"], {
     refetchOnWindowFocus: false,
     refetchInterval: false,
     refetchOnReconnect: false,
   });
 
   if (isLoading) {
-    console.log("Loading...");
-    return <p>Loading...</p>;
+    return <p className="text-center text-2xl font-bold">Loading hang on...</p>;
   }
 
   if (!data || error) {
-    return <p>Error</p>;
+    return (
+      <p className="text-center text-2xl font-bold">Could not get any mons </p>
+    );
   }
+
+  const makeGuess = () => {
+    if (value.toLocaleLowerCase() === data.name.toLocaleLowerCase()) {
+      setCorrect(true);
+
+      timerRef.current = setTimeout(() => {
+        setCorrect(false);
+        setValue("");
+        refetch();
+      }, 1000);
+    }
+  };
+
+  const skipGuess = () => {
+    clearInterval(timerRef.current);
+    setCorrect(false);
+    refetch();
+  };
 
   return (
     <div className="flex flex-col justify-center items-center gap-2">
-      <Pokemon {...data} />
+      <Pokemon {...data} correct={correct} />
+      <div className="flex items-center gap-5">
+        <input
+          type="text"
+          className={`border-2 p-2 rounded-md`}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button
+          className="text-2xl border-2 rounded-full w-10 h-10"
+          onClick={makeGuess}
+        >
+          &rarr;
+        </button>
+        <button onClick={skipGuess}>Skip</button>
+      </div>
     </div>
   );
 };
