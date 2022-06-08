51e34e30d3e608d4b513a1db1f163e9ea812c33f
Gerardo <gr261212@gmail.com>
2022-06-08 19:42:22 -0400

    get all pokemon

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -10,34 +10,54 @@ interface IPokemon {
   altName: string;
 }
 
-export const appRouter = trpc.router().query("get-pokemon", {
-  async resolve(): Promise<IPokemon> {
-    const prisma = new PrismaClient();
-    const id = Math.floor(Math.random() * 151) + 1;
+export const appRouter = trpc
+  .router()
+  .query("get-pokemon", {
+    async resolve(): Promise<IPokemon> {
+      const prisma = new PrismaClient();
+      const id = Math.floor(Math.random() * 151) + 1;
 
-    const pokemon = await prisma.pokemon.findUnique({
-      where: {
-        id,
-      },
-    });
-
-    if (!pokemon) {
-      throw new trpc.TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "An error occurred while fetching the pokemon",
+      const pokemon = await prisma.pokemon.findUnique({
+        where: {
+          id,
+        },
       });
-    }
 
-    // TODO: fix this: !
-    return {
-      id: pokemon.id,
-      name: pokemon.name,
-      sprite: pokemon.sprite!,
-      description: pokemon.description!,
-      altName: pokemon.altName,
-    };
-  },
-});
+      if (!pokemon) {
+        throw new trpc.TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "An error occurred while fetching the pokemon",
+        });
+      }
+
+      // TODO: fix this: !
+      return {
+        id: pokemon.id,
+        name: pokemon.name,
+        sprite: pokemon.sprite!,
+        description: pokemon.description!,
+        altName: pokemon.altName,
+      };
+    },
+  })
+  .query("get-all-pokemon", {
+    async resolve() {
+      const prisma = new PrismaClient();
+
+      const pokemon = await prisma.pokemon.findMany();
+
+      if (!pokemon) {
+        throw new trpc.TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "An error occurred while fetching all pokemon",
+        });
+      }
+
+      return {
+        pokemon,
+      };
+    },
+  });
 
 export type AppRouter = typeof appRouter;
 
