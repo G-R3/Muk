1728859164288149a3c531ab7d8baedaf48cb280
Gerardo <gr261212@gmail.com>
2022-06-11 14:29:40 -0400

    create or connect pokemon to a pokemon type

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -1,5 +1,6 @@
 import { PokemonClient } from "pokenode-ts";
 import { PrismaClient } from "@prisma/client";
+import { connect } from "http2";
 
 const prisma = new PrismaClient();
 const MAX_POKEMON = 151;
@@ -8,6 +9,20 @@ async function main() {
   const pokemonApi = new PokemonClient();
 
   console.log("Seeding database...");
+
+  // const { results } = await pokemonApi.listTypes(0);
+
+  // console.log("Creating types");
+  // for (const type of results) {
+  //   await prisma.pokemonType.create({
+  //     data: {
+  //       typeId: Number(type.url.split("/").slice(-2)[0]),
+  //       name: type.name,
+  //     },
+  //   });
+  // }
+
+  console.log("Creating Pokemons");
   for (let i = 1; i <= MAX_POKEMON; i++) {
     const pokemon = await pokemonApi.getPokemonById(i);
     const pokemonSpecies = await pokemonApi.getPokemonSpeciesById(i);
@@ -16,14 +31,24 @@ async function main() {
     const description = pokemonSpecies.flavor_text_entries
       .find((mon) => mon.language.name === "en")
       ?.flavor_text.replace(/\f/g, " ");
-
+    console.log(pokemon.types);
+    // types: [ {type: { name: "grass", url: "..."  }} ]
     await prisma.pokemon.create({
       data: {
-        id: pokemon.id,
         name: pokemon.name,
         sprite: sprite,
         description: description,
         altName: pokemonSpecies.names[0].name,
+        pokemonTypes: {
+          connectOrCreate: pokemon.types.map((type) => ({
+            where: {
+              name: type.type.name,
+            },
+            create: {
+              name: type.type.name,
+            },
+          })),
+        },
       },
     });
   }
